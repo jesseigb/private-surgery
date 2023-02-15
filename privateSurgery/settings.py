2372fd4fdd793bac3af992764d01709ce44697d6
jesseigb <77441355+jesseigb@users.noreply.github.com>
2023-02-15 21:54:10 +0000

    Database fixes

--- privateSurgery/settings.py
+++ privateSurgery/settings.py
@@ -14,6 +14,11 @@ from pathlib import Path
 import django_heroku
 import dj_database_url
 import os
+import environ
+
+# Initialise environment variables
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-n47qxf+ft7e90c2#k##oux=dbqf#^&m_f%^3#cf%9$&)plg^al'
+SECRET_KEY = env("KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
